0c3a569dcee5866ef307406a8298f51f2144d08e
Paul Williams <paul@paulswilliams.me.uk>
2016-01-30 21:36:56 +0000

    Introduced parts intermediary

--- src/gormanic/core.clj
+++ src/gormanic/core.clj
@@ -1,6 +1,6 @@
 (ns gormanic.core)
 
-(def months ["March",
+(def gormanic-months ["March",
              "April",
              "May",
              "June",
@@ -15,18 +15,40 @@
              "Gormanuary"])
 
 
-(defn day-of-month [day-of-year] (+ (rem (- day-of-year 1) 28) 1))
+(defn gormanic-day-index [day-of-year] (+ (rem (- day-of-year 1) 28) 1))
 
-(defn day-of-year [gregorian-date] (.get (.dayOfYear gregorian-date)))
+(defn gormanic-month-index [day-of-year] (quot (- day-of-year 1) 28))
 
-(defn month-index [day-of-year] (quot (- day-of-year 1) 28))
+(defn gregorian-to-parts [gregorian-date]
+  {:day (.get (.dayOfYear gregorian-date))
+   :year (.get (.year gregorian-date))})
 
-(defn year [gregorian-date] (.get (.year gregorian-date)))
+(defn gormanic-to-parts [gormanic]
+  {:day
+         (if [:intermission gormanic]
+          365
+          (.indexOf gormanic.core/gormanic-months
+                    (+
+                      (* 28 (:month gormanic))
+                      (:day gormanic)))
+          )
+   :year (:year gormanic)})
+
+(defn gormanic-to-string [gormanic]
+  (if (:intermission gormanic)
+    "Intermission"
+    (str (:day gormanic) " " (gormanic-months (:month gormanic)) " " (:year gormanic))))
+
+(defn parts-to-gormanic [parts]
+  (let [day (:day parts)]
+    (if (>= day 365)
+      {:year (:year parts) :intermission true}
+      {:year (:year parts)
+       :month (gormanic-month-index day)
+       :day (gormanic-day-index day)})))
 
 (defn convert
-  [gregorian-date]
-  (let [day (day-of-year gregorian-date)
-        month (month-index day)]
-    (if (< month 13)
-      (str (day-of-month day) " " (months month) " " (year gregorian-date))
-      "Intermission")))
+  [gregorian]
+  (gormanic-to-string
+    (parts-to-gormanic
+      (gregorian-to-parts gregorian))))
--- test/gormanic/core_test.clj
+++ test/gormanic/core_test.clj
@@ -8,19 +8,19 @@
     (is (= "1 March 2016" (convert (t/date-time 2016 1 1))))
     (is (= "1 April 2016" (convert (t/date-time 2016 1 29))))
     (is (= "2 April 2016" (convert (t/date-time 2016 1 30))))
-    (is (= "6 May 2016" (convert (t/date-time 2016 3 2))))    )
+    (is (= "6 May 2016" (convert (t/date-time 2016 3 2)))))
   (testing "non leap year"
     (is (= "1 April 2015" (convert (t/date-time 2015 1 29))))
     (is (= "5 May 2015" (convert (t/date-time 2015 3 2))))
     (is (= "1 February 2015" (convert (t/date-time 2015 11 5))))
     (is (= "7 June 2015" (convert (t/date-time 2015 4 1))))
-    (is (= "28 Gormanuary 2015" (convert (t/date-time 2015 12 30)))) 
-    (is (= "13 Gormanuary 2015" (convert (t/date-time 2015 12 15)))) 
+    (is (= "28 Gormanuary 2015" (convert (t/date-time 2015 12 30))))  
+    (is (= "13 Gormanuary 2015" (convert (t/date-time 2015 12 15))))  
     (is (= "Intermission" (convert (t/date-time 2015 12 31))))
     (is (= "Intermission" (convert (t/date-time 2015 12 31)))))
   (testing "post leap year")
-    (is (= "4 May 2016" (convert (t/date-time 2016 2 29)))) 
-    (is (= "2 February 2016" (convert (t/date-time 2016 11 5)))) 
-    (is (= "8 June 2016" (convert (t/date-time 2016 4 1)))) 
-    (is (= "Intermission" (convert (t/date-time 2016 12 30)))) 
-    (is (= "Intermission" (convert (t/date-time 2016 12 31)))))
+  (is (= "4 May 2016" (convert (t/date-time 2016 2 29))))  
+  (is (= "2 February 2016" (convert (t/date-time 2016 11 5))))  
+  (is (= "8 June 2016" (convert (t/date-time 2016 4 1))))  
+  (is (= "Intermission" (convert (t/date-time 2016 12 30))))  
+  (is (= "Intermission" (convert (t/date-time 2016 12 31)))))
