1e37a4edf595cd7a86810a88ce9040f6474ceb11
Paul Williams <paul@paulswilliams.me.uk>
2016-01-31 12:18:58 +0000

    Passing with gregorian -> gormanic -> roundtrip and ambigous intermissions on leap years

--- test/gormanic/core_test.clj
+++ test/gormanic/core_test.clj
@@ -1,26 +1,15 @@
 (ns gormanic.core-test
   (:require [clojure.test :refer :all]
             [gormanic.core :refer :all]
-            [clj-time.core :as t]))
+            [clj-time.coerce :as timec]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
 
-(deftest output-tests
-  (testing "pre leap year"
-    (is (= "1 March 2016" (gregorian-to-gormanic-string (t/date-time 2016 1 1))))
-    (is (= "1 April 2016" (gregorian-to-gormanic-string (t/date-time 2016 1 29))))
-    (is (= "2 April 2016" (gregorian-to-gormanic-string (t/date-time 2016 1 30))))
-    (is (= "6 May 2016" (gregorian-to-gormanic-string (t/date-time 2016 3 2)))))
-  (testing "non leap year"
-    (is (= "1 April 2015" (gregorian-to-gormanic-string (t/date-time 2015 1 29))))
-    (is (= "5 May 2015" (gregorian-to-gormanic-string (t/date-time 2015 3 2))))
-    (is (= "1 February 2015" (gregorian-to-gormanic-string (t/date-time 2015 11 5))))
-    (is (= "7 June 2015" (gregorian-to-gormanic-string (t/date-time 2015 4 1))))
-    (is (= "28 Gormanuary 2015" (gregorian-to-gormanic-string (t/date-time 2015 12 30))))  
-    (is (= "13 Gormanuary 2015" (gregorian-to-gormanic-string (t/date-time 2015 12 15))))  
-    (is (= "Intermission 2015" (gregorian-to-gormanic-string (t/date-time 2015 12 31))))
-    (is (= "Intermission 2015" (gregorian-to-gormanic-string (t/date-time 2015 12 31)))))
-  (testing "post leap year")
-  (is (= "4 May 2016" (gregorian-to-gormanic-string (t/date-time 2016 2 29))))  
-  (is (= "2 February 2016" (gregorian-to-gormanic-string (t/date-time 2016 11 5))))  
-  (is (= "8 June 2016" (gregorian-to-gormanic-string (t/date-time 2016 4 1))))  
-  (is (= "Intermission 2016" (gregorian-to-gormanic-string (t/date-time 2016 12 30))))  
-  (is (= "Intermission 2016" (gregorian-to-gormanic-string (t/date-time 2016 12 31)))))
+(defn timestamp-to-date [timestamp]
+   (.toLocalDate(timec/from-long (* timestamp 1000) )))
+
+(defspec dates-are-reversable 10000
+  (prop/for-all [d (gen/fmap timestamp-to-date
+                             (gen/choose -17987443200 19880899200))]
+                (= d (.toLocalDate (gormanic-to-gregorian (gregorian-to-gormanic d))))))
