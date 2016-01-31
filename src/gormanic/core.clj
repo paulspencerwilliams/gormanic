b6701dab312630b3ebb439a18e468d12e8667cfc
Paul Williams <paul@paulswilliams.me.uk>
2016-01-31 13:37:41 +0000

    Tests take into account intermission roundtrip ambiguity

--- src/gormanic/core.clj
+++ src/gormanic/core.clj
@@ -2,48 +2,47 @@
   (:require [clj-time.core :as t]))
 
 (def gormanic-months ["March",
-             "April",
-             "May",
-             "June",
-             "Quintilis",
-             "Sextilis",
-             "September",
-             "October",
-             "November",
-             "December",
-             "January",
-             "February",
-             "Gormanuary"])
+                      "April",
+                      "May",
+                      "June",
+                      "Quintilis",
+                      "Sextilis",
+                      "September",
+                      "October",
+                      "November",
+                      "December",
+                      "January",
+                      "February",
+                      "Gormanuary"])
 
 (defn gormanic-day-index [day-of-year] (+ (rem (- day-of-year 1) 28) 1))
 
 (defn gormanic-month-index [day-of-year] (+ (quot (- day-of-year 1) 28) 1))
 
 (defn gregorian-to-parts [gregorian-date]
-  {:day (.get (.dayOfYear gregorian-date))
+  {:day  (.get (.dayOfYear gregorian-date))
    :year (.get (.year gregorian-date))})
 
 (defn gormanic-to-parts [gormanic]
   {:day
          (if (:intermission gormanic)
-          365
-          (+ (* (- (:month gormanic) 1) 28) (:day gormanic)))
+           365
+           (+ (* (- (:month gormanic) 1) 28) (:day gormanic)))
    :year (:year gormanic)})
 
 (defn parts-to-gormanic [parts]
   (let [day (:day parts)]
     (if (>= day 365)
       {:year (:year parts) :intermission true}
-      {:year (:year parts)
+      {:year  (:year parts)
        :month (gormanic-month-index day)
-       :day (gormanic-day-index day)})))
+       :day   (gormanic-day-index day)})))
 
 (defn gormanic-to-string [gormanic]
   (if (:intermission gormanic)
     (str "Intermission " (:year gormanic))
     (str (:day gormanic) " "
-         (gormanic-months (- (:month gormanic) 1) ) " "
-         (:year gormanic))))
+         (gormanic-months (- (:month gormanic) 1)) " " (:year gormanic))))
 
 (defn gregorian-to-gormanic
   [gregorian]
@@ -55,7 +54,7 @@
   (gormanic-to-string (gregorian-to-gormanic gregorian)))
 
 (defn parts-to-gregorian [parts]
-  (.plusDays (t/date-time (:year parts) 1 1) (- (:day parts) 1) ))
+  (.plusDays (t/date-time (:year parts) 1 1) (- (:day parts) 1)))
 
 (defn gormanic-to-gregorian
   [gormanic]
--- test/gormanic/core_test.clj
+++ test/gormanic/core_test.clj
@@ -7,9 +7,24 @@
             [clojure.test.check.clojure-test :refer [defspec]]))
 
 (defn timestamp-to-date [timestamp]
-   (.toLocalDate(timec/from-long (* timestamp 1000) )))
+  (.toLocalDate (timec/from-long (* timestamp 1000))))
+
+(defn is2ndIntermission [d]
+  (and (= 12 (.get (.monthOfYear d)))
+       (= 31 (.get (.dayOfMonth d)))))
+
+(defn is1stIntermission [d]
+  (and (= 12 (.get (.monthOfYear d)))
+       (= 30 (.get (.dayOfMonth d)))))
+
+(defn equal-ignoring-leap-year-intermission [original roundtripped]
+  (or (= original (.toLocalDate roundtripped))
+      (and (is2ndIntermission original) (is1stIntermission roundtripped))))
 
 (defspec dates-are-reversable 10000
-  (prop/for-all [d (gen/fmap timestamp-to-date
-                             (gen/choose -17987443200 19880899200))]
-                (= d (.toLocalDate (gormanic-to-gregorian (gregorian-to-gormanic d))))))
+         (prop/for-all
+           [d (gen/fmap
+                timestamp-to-date
+                (gen/choose -17987443200 19880899200))]
+           (equal-ignoring-leap-year-intermission
+             d (gormanic-to-gregorian (gregorian-to-gormanic d)))))
